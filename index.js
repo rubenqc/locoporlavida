d7379d8abe85253826077c5b5a614928ef143b0e
rubenqc <rwquispec@gmail.com>
2021-11-13 20:57:12 -0500

    Added static files config

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res, next) => {
-  res.send('Hello world')
-})
+app.use(express.static('public'))
 
 app.listen(process.env.PORT, () => {
   console.log(`App started ${process.env.PORT}`)
